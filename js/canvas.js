2a90735e55ec876295af7dcd72801ac1fff0357f
jj5u <meltinp@gmail.com>
2023-01-24 21:25:39 +0900

    230124 add line width feature

--- js/canvas.js
+++ js/canvas.js
@@ -1,8 +1,9 @@
+const lineWidth = document.querySelector("#width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 300;
 canvas.height = 300;
-ctx.lineWidth = 2;
+ctx.lineWidth = lineWidth.value;
 let isPainting = false;
 
 function onMousemove(e){
@@ -18,8 +19,14 @@ function startPainting(){
 }
 function cancelPainting(){
     isPainting = false
+    ctx.beginPath()
+}
+function onLineWidthCahnge(e){
+    ctx.lineWidth = e.target.value
 }
 canvas.addEventListener('mousemove',onMousemove)
 canvas.addEventListener('mousedown',startPainting)
 canvas.addEventListener('mouseup',cancelPainting)
 canvas.addEventListener('mouseleave',cancelPainting)
+
+lineWidth.addEventListener('change', onLineWidthCahnge)
