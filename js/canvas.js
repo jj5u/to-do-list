ca94f2a6a0abe353c2833797629dd8b40c054790
jj5u <meltinp@gmail.com>
2023-01-24 19:38:07 +0900

    230124 freeline drawing canvas

--- js/canvas.js
+++ js/canvas.js
@@ -2,37 +2,24 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 300;
 canvas.height = 300;
-
-ctx.fillRect(100, 200, 25, 200);
-ctx.fillRect(200, 200, 25, 200);
 ctx.lineWidth = 2;
-ctx.strokeRect(150, 250, 25, 50);
-ctx.fillRect(100, 200, 100, 5);
-ctx.moveTo(100, 200);
-ctx.lineTo(162, 150);
-ctx.lineTo(225, 200);
-ctx.fill();
-ctx.beginPath();
-ctx.arc(169, 280, 3, 0, 2 * Math.PI);
-ctx.stroke();
+let isPainting = false;
 
-const colors = ['red','yellow','blue']
-let xPoint = ""
-let yPoint = ""
-
-function draw(e) {
-const color = colors[Math.floor(Math.random()*colors.length)]
-ctx.moveTo(xPoint,yPoint);
-ctx.lineTo(e.offsetX,e.offsetY);
-ctx.strokeStyle = color
-ctx.stroke();
-ctx.beginPath()
+function onMousemove(e){
+    if (isPainting) {
+        ctx.lineTo(e.offsetX, e.offsetY)
+        ctx.stroke()
+        return;
+    }
+    ctx.moveTo(e.offsetX, e.offsetY)
 }
-function onClick(e) {
-xPoint = e.offsetX
-yPoint = e.offsetY
-ctx.moveTo(xPoint,yPoint);
-canvas.addEventListener('mousemove',draw);
+function startPainting(){
+    isPainting = true
 }
-canvas.addEventListener('click',onClick)
-
+function cancelPainting(){
+    isPainting = false
+}
+canvas.addEventListener('mousemove',onMousemove)
+canvas.addEventListener('mousedown',startPainting)
+canvas.addEventListener('mouseup',cancelPainting)
+canvas.addEventListener('mouseleave',cancelPainting)
