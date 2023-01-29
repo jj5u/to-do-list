f331d51a0e2c26b3b8a0ad0ecfa80ffdb539ce94
Jahye Hwang <meltinp@gmail.com>
2023-01-29 22:20:08 +0900

    230129 add cavas features - added reset - added eraser

--- js/canvas.js
+++ js/canvas.js
@@ -3,17 +3,19 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 const color = document.getElementById("color");
 const colorOption = Array.from(document.getElementsByClassName("color-option"));
-const fillMode = document.querySelector(".fill-yn");
-canvas.width = 300;
-canvas.height = 300;
+const strokeModeBtn = document.querySelector(".mode-btn");
+const resetBtn = document.querySelector(".reset-btn");
+const eraserBtn = document.querySelector(".eraser-btn");
+const CANVAS_SIZE = 300;
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
 ctx.lineWidth = lineWidth.value;
 let isPainting = false;
-let fillModeYn = 1;
-
+let strokeMode = true;
+let eraserMode = false;
 function startPainting() {
-  ctx.beginPath();
   isPainting = true;
-  //   fillModeYn = 1;
+  ctx.beginPath();
 }
 function cancelPainting() {
   isPainting = false;
@@ -31,17 +33,30 @@ function onColorClick(e) {
   ctx.fillStyle = colorValue;
   color.value = colorValue;
 }
-function onFillMode(e) {
-  fillModeYn === 1 ? (fillModeYn = 0) : (fillModeYn = 1);
-  fillModeYn === 1 ? (e.target.innerText = "stroke-mode") : (e.target.innerText = "fill-mode");
+function onModeClick(e) {
+  ctx.strokeStyle = color.value;
+  strokeMode ? (strokeMode = false) : (strokeMode = true);
+  strokeMode ? (e.target.innerText = "stroke-mode") : (e.target.innerText = "fill-mode");
+  eraserMode = false ? (eraserBtn.innerText = "eraser") : (eraserBtn.innerText = "eraser");
 }
 function onMousemove(e) {
   if (isPainting) {
     ctx.lineTo(e.offsetX, e.offsetY);
-    fillModeYn === 1 ? ctx.stroke() : ctx.fill();
+    strokeMode ? ctx.stroke() : ctx.fill();
     return;
   }
   ctx.moveTo(e.offsetX, e.offsetY);
+}
+function resetCanvas() {
+  ctx.fillStyle = "#fff";
+  ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+}
+function eraseCanvas(e) {
+  strokeMode = true;
+  strokeModeBtn.innerText = "stroke-mode";
+  eraserMode = false ? (eraserMode = true) : (eraserMode = false);
+  eraserMode = false ? (eraserBtn.innerText = "eraser") : (eraserBtn.innerText = "eraser on");
+  eraserMode = false ? (ctx.strokeStyle = color.value) : (ctx.strokeStyle = "#fff");
 }
 canvas.addEventListener("mousemove", onMousemove);
 canvas.addEventListener("mousedown", startPainting);
@@ -49,7 +64,9 @@ canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
 lineWidth.addEventListener("change", onLineWidthChange);
 color.addEventListener("change", onColorChange);
-fillMode.addEventListener("click", onFillMode);
+strokeModeBtn.addEventListener("click", onModeClick);
 colorOption.forEach((color) => {
   color.addEventListener("click", onColorClick);
 });
+resetBtn.addEventListener("click", resetCanvas);
+eraserBtn.addEventListener("click", eraseCanvas);
