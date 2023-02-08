048b8a22c58c3fd0b159e63295e7ac33f7aacff8
Jahye Hwang <meltinp@gmail.com>
2023-02-09 00:22:00 +0900

    230209 add canvas features, clock css updated - text fill option added - can save canvas as .png - fill(paint) canvas with the button - font face option added - can change font size - can change text fill mode as either stroke or fill - no more wobbling clock

--- js/canvas.js
+++ js/canvas.js
@@ -2,18 +2,31 @@ const lineWidth = document.querySelector("#width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 const color = document.getElementById("color");
+const colorSelected = document.querySelector(".selected-color-option");
 const colorOption = Array.from(document.getElementsByClassName("color-option"));
 const strokeModeBtn = document.querySelector(".mode-btn");
 const resetBtn = document.querySelector(".reset-btn");
 const eraserBtn = document.querySelector(".eraser-btn");
+const saveBtn = document.querySelector(".save-btn");
 const inputImage = document.getElementById("file-image");
+const inputTxt = document.getElementById("input-txt");
+const radios = document.getElementsByName("font-style");
+const selectFontFace = document.getElementById("font-family");
+const selectFontSize = document.getElementById("font-size");
+const fillBtn = document.querySelector(".canvas-fill-btn");
 const CANVAS_SIZE = 300;
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
 ctx.lineWidth = lineWidth.value;
+ctx.lineCap = "round";
+colorSelected.dataset.color = color.value;
+colorSelected.style.backgroundColor = color.value;
 let isPainting = false;
 let strokeMode = true;
 let eraserMode = false;
+let fontStroke = true;
+let fontStyle = "";
+let fontSize = selectFontSize.value;
 function startPainting() {
   isPainting = true;
   ctx.beginPath();
@@ -26,13 +39,18 @@ function onLineWidthChange(e) {
   ctx.lineWidth = e.target.value;
 }
 function onColorChange(e) {
-  ctx.strokeStyle = e.target.value;
+  const colorInputValue = e.target.value;
+  ctx.strokeStyle = colorInputValue;
+  colorSelected.dataset.color = colorInputValue;
+  colorSelected.style.backgroundColor = colorInputValue;
 }
 function onColorClick(e) {
   const colorValue = e.target.dataset.color;
   ctx.strokeStyle = colorValue;
   ctx.fillStyle = colorValue;
   color.value = colorValue;
+  colorSelected.dataset.color = colorValue;
+  colorSelected.style.backgroundColor = colorValue;
 }
 function onModeClick(e) {
   ctx.strokeStyle = color.value;
@@ -69,6 +87,66 @@ function onFileChange(e) {
     inputImage.value = null;
   };
 }
+function paintText(e) {
+  const text = inputTxt.value;
+  ctx.lineWidth = 1;
+  const selectedStyle = `${fontSize}rem '${fontStyle}'`;
+  ctx.font = selectedStyle;
+  if (text !== "" && fontStroke) {
+    ctx.strokeText(text, e.offsetX, e.offsetY);
+  } else if (text !== "" && !fontStroke) {
+    ctx.fillText(text, e.offsetX, e.offsetY);
+  }
+}
+function onDbClick(e) {
+  ctx.save();
+  paintText(e);
+  ctx.restore();
+}
+function saveCanvas() {
+  const url = canvas.toDataURL();
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "my_img.png";
+  a.click();
+}
+function onRadioClick(e) {
+  if (e.target.value === "font-style-fill") {
+    fontStroke = false;
+  } else {
+    fontStroke = true;
+  }
+}
+function setFont(fontFamily, fontUrl) {
+  let fontFile = new FontFace(fontFamily, "url(" + fontUrl + ")");
+  document.fonts.add(fontFile);
+  fontFile.load();
+  fontStyle = fontFile.family;
+}
+function onSelectChange(e) {
+  const fontFamily = e.target.value;
+  const fontIndex = e.target.selectedIndex;
+  const fontUrlList = [
+    "",
+    "https://fonts.gstatic.com/s/sassyfrass/v5/LhWhMVrGOe0FLb97BjhsE-9aEtSygOan.woff2",
+    "https://fonts.gstatic.com/s/robotomono/v22/L0xuDF4xlVMF-BfR8bXMIhJHg45mwgGEFl0_3vq_ROW4AJi8SJQt.woff2",
+  ];
+  const fontUrl = fontUrlList[fontIndex];
+  setFont(fontFamily, fontUrl);
+}
+function onFontSizeChange(e) {
+  fontSize = e.target.value;
+}
+function paintCanvas() {
+  ctx.fillStyle = color.value;
+  ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+}
+function onFillBtnClick() {
+  ctx.save();
+  paintCanvas();
+  ctx.restore();
+}
+canvas.addEventListener("dblclick", onDbClick);
 canvas.addEventListener("mousemove", onMousemove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
@@ -79,6 +157,13 @@ strokeModeBtn.addEventListener("click", onModeClick);
 colorOption.forEach((color) => {
   color.addEventListener("click", onColorClick);
 });
+radios.forEach((radio) => {
+  radio.addEventListener("click", onRadioClick);
+});
 resetBtn.addEventListener("click", resetCanvas);
 eraserBtn.addEventListener("click", eraseCanvas);
+saveBtn.addEventListener("click", saveCanvas);
 inputImage.addEventListener("change", onFileChange);
+selectFontFace.addEventListener("change", onSelectChange);
+selectFontSize.addEventListener("change", onFontSizeChange);
+fillBtn.addEventListener("click", onFillBtnClick);
--- js/clock.js
+++ js/clock.js
@@ -6,7 +6,7 @@ function getClock() {
   const hours = date.getHours().toString().padStart(2, "0");
   const minutes = date.getMinutes().toString().padStart(2, "0");
   const seconds = date.getSeconds().toString().padStart(2, "0");
-  clock.innerHTML = `<span>${hours} <br> ${minutes} <br> ${seconds} </span>`;
+  clock.innerHTML = `<span>${hours}</span><br><span>${minutes}</span><br><span>${seconds}</span>`;
 }
 
 setInterval(getClock, 1000);
